0bcd6646e2c3b867b0066393b2658cfc646b5f52
Anahita N <anahita.nazari.99@gmail.com>
2025-12-29 19:57:36 +0330

    added sign up success page

--- app/signUpSuccess/page.js
+++ app/signUpSuccess/page.js
@@ -1,10 +1,12 @@
-import Image from 'next/image'
+import LogoTopBar from '../ui/LogoTopBar'
 
 export default function Success() {
-  return(<div className="mx-auto max-w-xl flex flex-col items-center mt-20 text-center glass-bg py-10 px-6 rounded-xl"> 
-<Image src={'/images/logo.svg'} width={120} height={23} alt='drop logo' className='mb-6'/>
+  return(<>
+  <LogoTopBar/>
+  <div className="mx-auto max-w-xl flex flex-col items-center mt-10 text-center glass-bg py-10 px-6 rounded-xl"> 
   <p className='text-header font-bold'>Your account was created Successfully🎉</p>
   <p className='text-body pt-12 pb-4'>before we start, Please set up your Profile</p>
   <a href="/setUpProfile"><button className="button pt-4">Set Up Profile</button></a>
-  </div>)
+  </div>
+  </>)
 }
