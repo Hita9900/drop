fe4d0592d2d05bd6763311ddb7d118f07f5ded07
Anahita N <anahita.nazari.99@gmail.com>
2025-12-30 16:07:55 +0330

    tweaking

--- app/ui/ClientAllTimeLeaderboard.js
+++ app/ui/ClientAllTimeLeaderboard.js
@@ -9,7 +9,7 @@ export default function ClientAllTimeLeaderboard({ users }) {
     return (
         <>
             <h2 className={`text-header mx-auto max-w-xl ${locale ==='fa'?'font-platinum':''}`}>{t('AllTimeTopPlayers')}</h2>
-            <p className={`text-small mx-auto max-w-xl ${locale ==='fa'?'font-yekan':''}`}>{t('RefreshText')}</p>
+            {/*<p className={`text-small mx-auto max-w-xl ${locale ==='fa'?'font-yekan':''}`}>{t('RefreshText')}</p>*/}
             <div className='mx-auto grid grid-cols-5 gap-0 max-w-xl'>
                 <div className={`col-1 flex items-center justify-center text-small opacity-40 h-10 ${locale ==='fa'?'font-yekan':''}`}>#</div>
                 <div className={`col-span-3 flex items-center text-samll opacity-40 ${locale ==='fa'?'font-yekan':''}`}>{t('Username')}</div>
